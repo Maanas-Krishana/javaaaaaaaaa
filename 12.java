623d5e98714813924a432f9109bede0883b6a1eb
Maanas Krishana <75085458+Maanas-Krishana@users.noreply.github.com>
2026-01-28 12:34:39 +0530

    Add DivisibleByFive class to check divisibility by 5

---
+++ 12.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class DivisibleByFive {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("ENTER NUM");
+            int num = sc.nextInt();
+            if (num % 5 == 0) {
+                System.out.println(num + " is divisible by 5");
+            } else {
+                System.out.println(num + " is not divisible by 5");
+            }
+        sc.close();
+    }
+}
