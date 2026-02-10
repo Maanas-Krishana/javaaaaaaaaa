f98c0c73eb5df178d8fb78122d63c427d84cf38d
Maanas Krishana <75085458+Maanas-Krishana@users.noreply.github.com>
2026-02-10 16:26:22 +0530

    Change printed value from j to 6-j

--- 25.java
+++ 25.java
@@ -4,7 +4,7 @@ public class Main
 		for(int i=1;i<=5;i++){
 		    for(int j=1;j<=5;j++){
 		        if(i>=j){
-		            System.out.print(j);
+		            System.out.print(6-j);
 		        }
 		        else{
 		            System.out.print(" ");
